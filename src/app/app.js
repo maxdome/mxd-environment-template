47e1e23ef68dab54dfb6d12e475a550ac6e12feb
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-29 14:32:49 +0200

    Add mxd-swagger

--- src/app/app.js
+++ src/app/app.js
@@ -2,5 +2,7 @@
 const { app, checkhelper, config, express, healthcheck, info, logging, silentLogging }
   = require('mxd-environment')();
 
+require('mxd-swagger')(app, express);
+
 app.use(require('./middlewares/example')());
 app.use(require('./routers/example')(express));
